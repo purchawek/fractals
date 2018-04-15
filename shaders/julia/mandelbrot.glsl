ad18246d8c5ab94ad141d32c3565c052392e3ba5
Adam Budziak <tajemniczyprecel@gmail.com>
2018-04-15 18:30:51 +0200

    simple mandelbrot

--- shaders/julia/mandelbrot.glsl
+++ shaders/julia/mandelbrot.glsl
@@ -4,23 +4,23 @@ uniform vec2 offset;
 uniform float factor;
 uniform vec2 z0;
 
-
 vec3 iters(float x, float y) {
-    float zx = 0.0;
-    float zy = 0.0;
+    float zx = z0[0];
+    float zy = z0[1];
     float result = 0.0;
     float tmp;
     for(; result < 100.0; ++result) {
-        if ((zx * zx + zy * zy) >= 4.0) return vec3(result, x, y);
+        if ((zx * zx + zy * zy) >= 4.0) return vec3(result, zx, zy);
         tmp = zx;
-        x = zx*zx - zy*zy + x;
-        y = 2.0 * tmp * zy + y;
+        zx = zx*zx - zy*zy + x;
+        zy = 2.0 * tmp * zy + y;
     }
-    return vec3(result, x, y);
+    return vec3(result, zx, zy);
 }
 
 vec4 get_color(vec3 iter, float max_iter) {
-    float div = iter[0] / max_iter;
+    float modulus = sqrt(iter[1]*iter[1] + iter[2]*iter[2]);
+    float div = (iter[0] - log(log(modulus)))/100.0;
     float color_r = div * color[0];
     float color_g = div * color[1];
     float color_b = div * color[2];
